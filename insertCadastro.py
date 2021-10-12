aac604ba5784c383cf8bb4682a614f7efaff621d
yuryrodri <yuryrodri@hotmail.com>
2021-10-12 14:33:52 -0300

    Cadastrados usuários no banco e suas digitais

--- insertCadastro.py
+++ insertCadastro.py
@@ -10,8 +10,7 @@ def convertToBinary(filePath):
 def insertBlob(nmCadastro,nvCadastro,pathDigital):
     try:
         #Conectar ao banco
-        conexaoSQLite = sqlite3.connect("C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
-                        "Semestre\\Projeto\\python-fingerprint\\db.sqlite3")
+        conexaoSQLite = sqlite3.connect('db.sqlite3')
         cursor = conexaoSQLite.cursor()
         print("Conectado com o banco SQLite")
 
@@ -34,5 +33,25 @@ def insertBlob(nmCadastro,nvCadastro,pathDigital):
             conexaoSQLite.close()
             print("Conexão com banco fechada")
 
-insertBlob("Ministro",3,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+insertBlob("Ministro Cesar Xavier",3,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
                         "Semestre\\Projeto\\python-fingerprint\\fingerprints\\101\\101_1.tif")
+
+insertBlob("Yury",2,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\102\\102_2.tif")
+insertBlob("Carlitos",2,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\103\\103_1.tif")
+insertBlob("Alysson",2,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\104\\104_1.tif")
+insertBlob("Fabrício",2,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\105\\105_7.tif")
+insertBlob("Augusto",2,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\106\\106_6.tif")
+
+insertBlob("Funcionário 1",1,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\107\\107_7.tif")
+insertBlob("Funcionário 2",1,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\108\\108_6.tif")
+insertBlob("Funcionário 3",1,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\109\\109_3.tif")
+insertBlob("Funcionário 4",1,"C:\\Yury\\UNIP\\6º Semestre\\PIVC - Processamento de Imagens e Visão Computacional\\APS 6º "
+                        "Semestre\\Projeto\\python-fingerprint\\fingerprints\\110\\110_3.tif")
