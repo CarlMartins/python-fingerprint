6d8410cc9f493af317d4cba1b923338e2fc544fc
yuryrodri <yuryrodri@hotmail.com>
2021-10-11 21:16:19 -0300

    Arrumada referencia a arquivos nas páginas html

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/templates/'
 
+STATICFILES_DIRS = ('/helloworld/static',)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
