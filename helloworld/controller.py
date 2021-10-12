fa50dd577d5079f78706041c6bc71157167e95fc
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-10-12 17:05:27 -0300

    Pagina index bonita

--- helloworld/controller.py
+++ helloworld/controller.py
@@ -6,6 +6,9 @@ import numpy as np
 @csrf_exempt
 def logar(request):
     digitalLogin = request.POST.get('imgDigital')
+    x = np.frombuffer((request.FILES.get('imgDigital').file), np.uint8)
+    imagem = cv2.imdecode(x, cv2.IMREAD_UNCHANGED)
+    cv2.imshow(imagem)
 
     try:
         # Conectar ao banco
