874384db18cb76999fd39d261b1d5edc2058b15f
yuryrodri <yuryrodri@hotmail.com>
2021-10-23 15:39:57 -0300

    Criadas classes de tratamento de imagem

--- helloworld/controller.py
+++ helloworld/controller.py
@@ -3,12 +3,45 @@ import sqlite3
 import cv2
 import numpy as np
 
+from helloworld.tratamentoImagem.normalizacao import normalize
+from helloworld.tratamentoImagem.segmentacao import create_segmented_and_variance_images
+from helloworld.tratamentoImagem.calculoAngulos import *
+from helloworld.tratamentoImagem.frequencia import ridge_freq
+from helloworld.tratamentoImagem.filtroGabor import gabor_filter
+from helloworld.tratamentoImagem.esqueletizacao import skeletonize
+from helloworld.tratamentoImagem.crossing_number import calculate_minutiaes
+
+
+
+
 @csrf_exempt
 def logar(request):
-    digitalLogin = request.POST.get('imgDigital')
-    x = np.asarray(bytearray(request.FILES.get('imgDigital').read()), dtype=np.uint8)
-    imagem = cv2.imdecode(x, cv2.IMREAD_UNCHANGED)
-    cv2.imshow('Digital', imagem)
+    bytesDigitalLogin = np.asarray(bytearray(request.FILES.get('imgDigital').read()), dtype=np.uint8)
+    digitalLogin = cv2.imdecode(bytesDigitalLogin, 0) #cv2.IMREAD_UNCHANGED
+
+    block_size = 16
+    imagem = digitalLogin
+    imagemNormalizada = normalize(imagem, float(100), float(100))
+    _, threshold_im = cv2.threshold(imagemNormalizada, 127, 255, cv2.THRESH_OTSU)
+    (segmented_img, normim, mask) = create_segmented_and_variance_images(imagemNormalizada, block_size, 0.3)
+    imagemNormalizada = cv2.GaussianBlur(imagemNormalizada, (9, 9), 0)
+    angles = calculate_angles(imagemNormalizada, W=block_size, smoth=False)
+    orientation_img = visualize_angles(segmented_img, mask, angles, W=block_size)
+    freq = ridge_freq(normim, mask, angles, block_size, kernel_size=5, minWaveLength=5, maxWaveLength=15)
+    gabor_img = gabor_filter(normim, angles, freq)
+    skel_img = skeletonize(gabor_img)
+    minutias = calculate_minutiaes(skel_img,kernel_size=5)
+
+    cv2.imshow("teste", imagemNormalizada)
+    cv2.imshow("teste2", threshold_im)
+    cv2.imshow("teste3", segmented_img)
+    cv2.imshow("teste4", normim)
+    cv2.imshow("teste5", orientation_img)
+    cv2.imshow("teste6", freq)
+    cv2.imshow("teste7", gabor_img)
+    cv2.imshow("teste8", skel_img)
+    cv2.imshow("teste9", minutias)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -35,10 +68,20 @@ def logar(request):
 def comparaDigitais(digitalLogin,resultados):
     for linha in resultados:
         idCadastro = linha[0]
-        digitalBanco = linha[1]
+        imgDigital = linha[1]
         nmCadastro = linha[2]
         nvCadastro = linha[3]
 
         #Algorítmo de comparação
+        bytesDigitalBanco = np.frombuffer(imgDigital, dtype='uint8')
+
+        # decode the array into an image
+        digitalBanco = cv2.imdecode(bytesDigitalBanco, cv2.IMREAD_UNCHANGED)
+
+        #digitalBanco => digitalBanco já lida pelo openCV
+        #digitalLogin => digitalLogin já lida pelo openCV
+
+
+
 
     return None
