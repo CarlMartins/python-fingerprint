7028503118a909efe4ba6523c99945753c8971a5
yuryrodri <yuryrodri@hotmail.com>
2021-10-25 08:04:27 -0300

    Adicionado leitura de retorno dos processos executados em multithreading

--- helloworld/controller.py
+++ helloworld/controller.py
@@ -38,28 +38,36 @@ def logar(request):
 
 def comparaDigitais(img_minutias_login, descriptorLogin, resultados):
     threads = []
+    pipes = []
     for linha in resultados:
-        idCadastro = linha[0]
-        imgDigital = linha[1]
-        nmCadastro = linha[2]
-        nvCadastro = linha[3]
+        id_cadastro = linha[0]
+        img_digital = linha[1]
+        nm_cadastro = linha[2]
+        nv_cadastro = linha[3]
 
-        bytesDigitalBanco = np.frombuffer(imgDigital, dtype='uint8')
+        bytesDigitalBanco = np.frombuffer(img_digital, dtype='uint8')
 
         # decode the array into an image
         digitalBanco = cv2.imdecode(bytesDigitalBanco, 0)
 
-        t = multiprocessing.Process(target=comparar, args=(img_minutias_login, descriptorLogin, digitalBanco))
+        pipe_pai,pipe_filho = multiprocessing.Pipe()
+        returns = []
+
+        t = multiprocessing.Process(target=comparar, args=(img_minutias_login, descriptorLogin, id_cadastro,digitalBanco,pipe_filho))
         t.start()
+        pipes.append(pipe_pai)
         threads.append(t)
 
     for thread in threads:
         thread.join()
 
+    for pipe in pipes:
+        print(pipe.recv()[1])
+
     return None
 
 
-def comparar(img_minutias_login, descriptor_login, digital_banco):
+def comparar(img_minutias_login, descriptor_login,id_cadastro, digital_banco,pipe_filho):
     minutias_banco, descriptor_banco, img_minutias_banco = extraiMinutias(digital_banco)
 
     # Matching between descriptors
@@ -79,6 +87,10 @@ def comparar(img_minutias_login, descriptor_login, digital_banco):
     score_threshold = 40
     print(score / len(matches))
     if score / len(matches) < score_threshold:
-        print("Fingerprint matches.")
+        print("Digital compatível")
+        pipe_filho.send([id_cadastro,True])
+        pipe_filho.close()
     else:
-        print("Fingerprint does not match.")
+        print("Digital incompatível.")
+        pipe_filho.send([id_cadastro,False])
+        pipe_filho.close()
